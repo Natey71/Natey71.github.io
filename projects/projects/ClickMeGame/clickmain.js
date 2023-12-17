6ccd4e637768416af7d95c462d85c22ae41d084d
Nathan <natedog7120@gmail.com>
2023-12-16 22:09:04 -0600

    done for the day

--- projects/projects/ClickMeGame/clickmain.js
+++ projects/projects/ClickMeGame/clickmain.js
@@ -9,17 +9,24 @@ function manualIncrement() {
 
 
 function autoIncrement(amount) {
+    // Update the increment rate
+    if(clickCount >= amount) {
+
+        clickCount -= amount;
+        document.getElementById("clicks").textContent = clickCount; 
+        currentIncrementRate += amount;
+
+        // If there's no interval running, start one
+        if (incrementInterval === null) {
+            incrementInterval = setInterval(function() {
+                clickCount += currentIncrementRate;
+                document.getElementById("clicks").textContent = clickCount;
+            }, 1000); // Update every second
+    }
+    } else {
+        const message = "You do not have enough money \n Amount needed: " + amount + "\n\n Your current bank account: " + clickCount;
+        alert(message);
+    }
     
-
- // Update the increment rate
- currentIncrementRate += amount;
-
- // If there's no interval running, start one
- if (incrementInterval === null) {
-     incrementInterval = setInterval(function() {
-        clickCount += currentIncrementRate;
-         document.getElementById("clicks").textContent = clickCount;
-     }, 1000); // Update every second
- }
 
 }
